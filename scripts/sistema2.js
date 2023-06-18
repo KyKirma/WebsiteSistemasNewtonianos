23d01622da3058a8ed72eae4bb6c5e92e65d2041
KyKirma <pedrohenriquekourly@hotmail.com>
2023-06-18 17:12:34 -0300

    Att layout sistemas

--- scripts/sistema2.js
+++ scripts/sistema2.js
@@ -7,6 +7,6 @@ function calcular() {
   var aceleracao = pesoB / (massaA + massaB);
   var tensaoFio1 = massaA * aceleracao;
 
-  document.getElementById("resultado").innerHTML = `<b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i><br>
-  <b> Tensão do Fio 1:</b> ${tensaoFio1.toFixed(2)} <i>N</i>`;
+  document.getElementById("resultado").innerHTML = `<li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i></li><br>
+  <li><b> Tensão do Fio 1:</b> ${tensaoFio1.toFixed(2)} <i>N</i></li>`;
 }
