aed282207a7c3330f369dcc68bab886fac58c89c
KyKirma <pedrohenriquekourly@hotmail.com>
2023-06-22 10:13:33 -0300

    Update Sistema I e VI implementados

--- scripts/Sistema1.js
+++ scripts/Sistema1.js
@@ -3,31 +3,54 @@ function calcular() {
   var massa2 = parseFloat(document.getElementById("massa2").value);
   var massa3 = parseFloat(document.getElementById("massa3").value);
   var forca = parseFloat(document.getElementById("forca").value);
-  var coeficiente = parseFloat(document.getElementById("coeficiente").value);
+  var coeficiente = parseFloat(document.getElementById("atrito").value);
   var gravidade = parseFloat(document.getElementById("gravidade").value);
   var tempo = parseFloat(document.getElementById("tempo").value);
+  var respostaAtrito = document.getElementById("checkAtrito");
 
-  var forcaAtrito1 = (coeficiente * massa1 * gravidade).toFixed(2);
-  var forcaAtrito2 = (coeficiente * massa2 * gravidade).toFixed(2);
-  var forcaAtrito3 = (coeficiente * massa3 * gravidade).toFixed(2);
+  if (respostaAtrito.checked) {
+    //com atrito
+    var forcaAtrito1 = (coeficiente * massa1 * gravidade);
+    var forcaAtrito2 = (coeficiente * massa2 * gravidade);
+    var forcaAtrito3 = (coeficiente * massa3 * gravidade);
 
-  var aceleracao = ((forca - (parseFloat(forcaAtrito1) + parseFloat(forcaAtrito2) + parseFloat(forcaAtrito3))) / (massa1 + massa2 + massa3)).toFixed(2);
-  aceleracao = aceleracao < 0 ? 0 : aceleracao; // Define a aceleração como zero se for negativa
+    var aceleracao = ((forca - (parseFloat(forcaAtrito1) + parseFloat(forcaAtrito2) + parseFloat(forcaAtrito3))) / (massa1 + massa2 + massa3));
+    aceleracao = aceleracao < 0 ? 0 : aceleracao; // Define a aceleração como zero se for negativa
 
-  var tracao1 = ((massa1 * aceleracao) + parseFloat(forcaAtrito1)).toFixed(2);
-  var tracao2 = ((massa2 * aceleracao) + parseFloat(forcaAtrito2) + parseFloat(tracao1)).toFixed(2);
+    var tracao1 = ((massa1 * aceleracao) + parseFloat(forcaAtrito1));
+    var tracao2 = ((massa2 * aceleracao) + parseFloat(forcaAtrito2) + parseFloat(tracao1));
 
-  var velocidade = (aceleracao * tempo).toFixed(2);
-  var distancia = ((aceleracao * Math.pow(tempo, 2)) / 2).toFixed(2);
+    var velocidade = (aceleracao * tempo);
+    var distancia = ((aceleracao * Math.pow(tempo, 2)) / 2);
 
-  console.log("Força de Atrito 1:", forcaAtrito1);
-  console.log("Força de Atrito 2:", forcaAtrito2);
-  console.log("Força de Atrito 3:", forcaAtrito3);
-  console.log("Aceleração (valor absoluto):", aceleracao);
-  console.log("Tração 1:", tracao1);
-  console.log("Tração 2:", tracao2);
-  console.log("Velocidade:", velocidade);
-  console.log("Distância:", distancia);
+    document.getElementById("resultado").innerHTML = `<li><b>Atrito Bloco 1:</b> ${forcaAtrito1.toFixed(2)} <i>N</i></li><br>
+    <li><b>Atrito Bloco 2:</b> ${forcaAtrito2.toFixed(2)} <i>N</i></li><br>
+    <li><b>Atrito Bloco 3:</b> ${forcaAtrito3.toFixed(2)} <i>N</i></li><br>
+    <li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i></li><br>
+    <li><b>Tensão do Fio 1:</b> ${tracao1.toFixed(2)} <i>N</i></li><br>
+    <li><b>Tensão do Fio 2:</b> ${tracao2.toFixed(2)} <i>N</i></li><br>
+    <li><b>Velocidade final:</b> ${velocidade.toFixed(2)} <i>m/s</i></li><br>
+    <li><b>Distância percorrida:</b> ${distancia.toFixed(2)} <i>m</i></li><br>`;
+
+  } else {
+    //sem atrito
+
+    var aceleracao = (forca / (massa1 + massa2 + massa3));
+    aceleracao = aceleracao < 0 ? 0 : aceleracao; // Define a aceleração como zero se for negativa
+
+    var tracao1 = (massa1 * aceleracao);
+    var tracao2 = ((massa2 * aceleracao) + parseFloat(tracao1));
+
+    var velocidade = (aceleracao * tempo);
+    var distancia = ((aceleracao * Math.pow(tempo, 2)) / 2);
+
+    document.getElementById("resultado").innerHTML = `
+    <li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i></li><br>
+    <li><b>Tensão do Fio 1:</b> ${tracao1.toFixed(2)} <i>N</i></li><br>
+    <li><b>Tensão do Fio 2:</b> ${tracao2.toFixed(2)} <i>N</i></li><br>
+    <li><b>Velocidade final:</b> ${velocidade.toFixed(2)} <i>m/s</i></li><br>
+    <li><b>Distância percorrida:</b> ${distancia.toFixed(2)} <i>m</i></li><br>`;
+  }
 }
   
   
--- scripts/sistema6.js
+++ scripts/sistema6.js
@@ -1,37 +1,57 @@
-function calcularAceleracao() {
-  var massaBloco1 = parseFloat(document.getElementById('massaBloco1').value);
-  var massaBloco2 = parseFloat(document.getElementById('massaBloco2').value);
-  var massaBloco3 = parseFloat(document.getElementById('massaBloco3').value);
+function calcular() {
+  var massaBloco1 = parseFloat(document.getElementById('massa1').value);
+  var massaBloco2 = parseFloat(document.getElementById('massa2').value);
+  var massaBloco3 = parseFloat(document.getElementById('massa3').value);
   var gravidade = parseFloat(document.getElementById('gravidade').value);
-  var coeficienteAtrito = parseFloat(document.getElementById('coeficienteAtrito').value);
+  var coeficienteAtrito = parseFloat(document.getElementById('atrito').value);
   var forca = parseFloat(document.getElementById('forca').value);
+  var respostaAtrito = document.getElementById("checkAtrito");
 
   var pesoBloco1 = massaBloco1 * gravidade;
   var pesoBloco2 = massaBloco2 * gravidade;
   var pesoBloco3 = massaBloco3 * gravidade;
 
-  console.log('Peso do Bloco 1:', pesoBloco1.toFixed(2));
-  console.log('Peso do Bloco 2:', pesoBloco2.toFixed(2));
-  console.log('Peso do Bloco 3:', pesoBloco3.toFixed(2));
-
-  var forcaAtritoBloco1 = coeficienteAtrito * massaBloco1 * gravidade;
-  var forcaAtritoBloco2 = coeficienteAtrito * massaBloco2 * gravidade;
-  var forcaAtritoBloco3 = coeficienteAtrito * massaBloco3 * gravidade;
-
-  console.log('Força de Atrito do Bloco 1:', forcaAtritoBloco1.toFixed(2));
-  console.log('Força de Atrito do Bloco 2:', forcaAtritoBloco2.toFixed(2));
-  console.log('Força de Atrito do Bloco 3:', forcaAtritoBloco3.toFixed(2));
-
-  var somaForcasAtrito = forcaAtritoBloco1 + forcaAtritoBloco2 + forcaAtritoBloco3;
   var somaMassas = massaBloco1 + massaBloco2 + massaBloco3;
-  var aceleracao = (forca - somaForcasAtrito) / somaMassas;
+
+  if (respostaAtrito.checked) {
+    //com atrito
+    var forcaAtritoBloco1 = coeficienteAtrito * massaBloco1 * gravidade;
+    var forcaAtritoBloco2 = coeficienteAtrito * massaBloco2 * gravidade;
+    var forcaAtritoBloco3 = coeficienteAtrito * massaBloco3 * gravidade;
+
+    var somaForcasAtrito = forcaAtritoBloco1 + forcaAtritoBloco2 + forcaAtritoBloco3;
+
+    var aceleracao = (forca - somaForcasAtrito) / somaMassas;
   aceleracao = aceleracao < 0 ? 0 : aceleracao; // Define a aceleração como zero se for negativa
 
-  console.log('Aceleração do Sistema:', aceleracao.toFixed(2));
+    var forcaAB = forca - forcaAtritoBloco1 - (massaBloco1 * aceleracao);
+    var forcaBC = (massaBloco3 * aceleracao) + forcaAtritoBloco3;
 
-  var forcaAB = forca - forcaAtritoBloco1 - (massaBloco1 * aceleracao);
-  var forcaBC = (massaBloco3 * aceleracao) + forcaAtritoBloco3;
+    document.getElementById("resultado").innerHTML = `
+    <li><b>Peso Bloco 1:</b> ${pesoBloco1.toFixed(2)} <i>N</i></li><br>
+    <li><b>Peso Bloco 2:</b> ${pesoBloco2.toFixed(2)} <i>N</i></li><br>
+    <li><b>Peso Bloco 3:</b> ${pesoBloco3.toFixed(2)} <i>N</i></li><br>
+    <li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i></li><br>
+    <li><b>Força de Atrito no Bloco 1:</b> ${forcaAtritoBloco1.toFixed(2)} <i>N</i></li><br>
+    <li><b>Força de Atrito no Bloco 2:</b> ${forcaAtritoBloco2.toFixed(2)} <i>N</i></li><br>
+    <li><b>Força de Atrito no Bloco 3:</b> ${forcaAtritoBloco3.toFixed(2)} <i>N</i></li><br>
+    <li><b>Força de 1 sobre 2:</b> ${forcaAB.toFixed(2)} <i>m/s</i></li><br>
+    <li><b>Força de 2 sobre 3:</b> ${forcaBC.toFixed(2)} <i>m/s</i></li><br>`;
 
-  console.log('Força AB:', forcaAB.toFixed(2));
-  console.log('Força BC:', forcaBC.toFixed(2));
+  } else {
+    //sem atrito
+
+    var aceleracao = forca / somaMassas;
+  aceleracao = aceleracao < 0 ? 0 : aceleracao; // Define a aceleração como zero se for negativa
+    var forcaAB = forca - (massaBloco1 * aceleracao);
+    var forcaBC = (massaBloco3 * aceleracao);
+
+    document.getElementById("resultado").innerHTML = `
+    <li><b>Peso Bloco 1:</b> ${pesoBloco1.toFixed(2)} <i>N</i></li><br>
+    <li><b>Peso Bloco 2:</b> ${pesoBloco2.toFixed(2)} <i>N</i></li><br>
+    <li><b>Peso Bloco 3:</b> ${pesoBloco3.toFixed(2)} <i>N</i></li><br>
+    <li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i></li><br>
+    <li><b>Força de 1 sobre 2:</b> ${forcaAB.toFixed(2)} <i>m/s</i></li><br>
+    <li><b>Força de 2 sobre 3:</b> ${forcaBC.toFixed(2)} <i>m/s</i></li><br>`;
+  }
 }
