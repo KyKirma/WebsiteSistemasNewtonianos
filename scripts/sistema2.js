5eecde30afd6b3427794306d29688b8c22ae7e2e
KyKirma <pedrohenriquekourly@hotmail.com>
2023-06-23 10:55:51 -0300

    Update Script Sistema 2

--- scripts/sistema2.js
+++ scripts/sistema2.js
@@ -7,23 +7,39 @@ function calcular() {
 
   var pesoB = massaB * gravidade;
   
+  var explicacao = "";
+
   if (respostaAtrito.checked) {
     // Se houver atrito
     var coeficienteAtrito = parseFloat(document.getElementById("atrito").value);
 
-    // Cálculo da aceleração com atrito
-    var aceleracaoComAtrito = (pesoB - coeficienteAtrito * massaA * gravidade) / (massaA + massaB);
-    var tensaoFio1ComAtrito = massaA * aceleracaoComAtrito;
+    // Verifica se a força aplicada é menor que a força de atrito estático máximo
+    if (pesoB < coeficienteAtrito * massaA * gravidade) {
+      aceleracao = 0.0;
+      explicacao = "Foi considerada a aceleração como zero, pois o valor da força aplicada no sistema é menor que a força de atrito estático máximo. Então os corpos permanecerão em repouso.";
 
-    // Cálculo da velocidade e deslocamento
-    var velocidadeAtrito = aceleracaoComAtrito * tempo;
-    var deslocamento = 0.5 * aceleracaoComAtrito * Math.pow(tempo, 2);
+    } else {
 
-    // Saída com os resultados considerando o atrito, velocidade e deslocamento
-    document.getElementById("resultado").innerHTML = `<li><b>Aceleração do sistema:</b> ${aceleracaoComAtrito.toFixed(2)} <i>m/s²</i></li><br>
-    <li><b>Tensão do Fio:</b> ${tensaoFio1ComAtrito.toFixed(2)} <i>N</i></li><br>
-    <li><b>Velocidade final:</b> ${velocidadeAtrito.toFixed(2)} <i>m/s</i></li><br>
-    <li><b>Distância percorrida:</b> ${deslocamento.toFixed(2)} <i>m</i></li><br>`;
+      // Cálculo da aceleração com atrito
+      var aceleracaoComAtrito = (pesoB - coeficienteAtrito * massaA * gravidade) / (massaA + massaB);
+      var tensaoFio1ComAtrito = massaA * aceleracaoComAtrito;
+
+
+      if (aceleracaoComAtrito < 0) {
+        aceleracaoComAtrito = 0.0;
+        explicacao = "Foi considerada a aceleração como zero, pois o valor da força aplicada no sistema é menor que a força de atrito estático máximo. Então os corpos permanecerão em repouso.";
+      }
+      // Cálculo da velocidade e deslocamento
+      var velocidadeAtrito = aceleracaoComAtrito * tempo;
+      var deslocamento = 0.5 * aceleracaoComAtrito * Math.pow(tempo, 2);
+
+      // Saída com os resultados considerando o atrito, velocidade e deslocamento
+      document.getElementById("resultado").innerHTML = `<li><b>Aceleração do sistema:</b> ${aceleracaoComAtrito.toFixed(2)} <i>m/s²</i><br>${explicacao}</li><br>
+      <li><b>Tensão do Fio:</b> ${tensaoFio1ComAtrito.toFixed(2)} <i>N</i></li><br>
+      <li><b>Velocidade final:</b> ${velocidadeAtrito.toFixed(2)} <i>m/s</i></li><br>
+      <li><b>Distância percorrida:</b> ${deslocamento.toFixed(2)} <i>m</i></li><br>`;
+      
+    }
   } else {
     // Caso não haja atrito
     var aceleracao = pesoB / (massaA + massaB);
@@ -31,7 +47,12 @@ function calcular() {
     var velocidadeFinal = aceleracao * tempo;
     var deslocamentoFinal = 0.5 * aceleracao * Math.pow(tempo, 2);
 
-    document.getElementById("resultado").innerHTML = `<li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i></li><br>
+    if (aceleracao < 0) {
+      aceleracao = 0.0;
+      explicacao = "Foi considerada a aceleração como zero, pois o valor da força aplicada no sistema é menor que a força de atrito estático máximo. Então os corpos permanecerão em repouso.";
+    }
+
+    document.getElementById("resultado").innerHTML = `<li><b>Aceleração do sistema:</b> ${aceleracao.toFixed(2)} <i>m/s²</i><br>${explicacao}</li><br>
     <li><b>Tensão do Fio:</b> ${tensaoFio1.toFixed(2)} <i>N</i></li><br>
     <li><b>Velocidade final:</b> ${velocidadeFinal.toFixed(2)} <i>m/s</i></li><br>
     <li><b>Distância percorrida:</b> ${deslocamentoFinal.toFixed(2)} <i>m</i></li><br>`;
